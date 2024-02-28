ff25b25991cf1dea67d24b6add3a888d37a49975
Zalera <cjpff14@gmail.com>
2024-02-28 14:54:35 -0500

    woops

--- Youtube-Transcript-Grabber/Program.cs
+++ Youtube-Transcript-Grabber/Program.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using YoutubeTranscriptApi;
 
-public class YouTubeTranscriptFetcher
+public class YouTubeTranscriptGrabber
 {
 
     static void Main()
